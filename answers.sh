3bc5f6a8ef200c0be0d71bbd1209324fe36fd8e7
Rolamaalouf <roula.maalouf@hotmail.fr>
2024-12-11 10:54:00 +0200

    exercise 3

--- answers.sh
+++ answers.sh
@@ -11,3 +11,7 @@ exercise 2
 INSERT INTO graduates (Name,Age,Gender,points) SELECT Name, Age, Gender, Points FROM students WHERE Name = 'layal';
 UPDATE graduates SET Graduation = 'August-09-2018' WHERE name = 'Layal'
 DELETE FROM students WHERE name='Layal'
+exercise 3
+SELECT employees.Name  , employees.Company  ,companies.Date from employees,companies where employees.Company=companies.Name
+SELECT employees.Name from employees,companies where employees.Company=companies.Name and companies.Date<2000
+select Company from employees WHERE role='Graphic Designer'
