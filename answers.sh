b2a223e86c0aea2b99d8e2d1509b66608115a504
Rolamaalouf <roula.maalouf@hotmail.fr>
2024-12-11 09:56:28 +0200

    answer 2

--- answers.sh
+++ answers.sh
@@ -1,3 +1,4 @@
+exercise 1 
 select * from students
 select name from students
 SELECT * FROM students WHERE age > 30;
@@ -6,3 +7,7 @@ SELECT points FROM students WHERE name = 'Alex'
 INSERT INTO students (name, age , Gender) VALUES ('rola', 39, 'F');
 UPDATE students SET points = points + 30 WHERE name = 'Basma'
 UPDATE students SET points = points - 10 WHERE name = 'Alex'
+exercise 2 
+INSERT INTO graduates (Name,Age,Gender,points) SELECT Name, Age, Gender, Points FROM students WHERE Name = 'layal';
+UPDATE graduates SET Graduation = 'August-09-2018' WHERE name = 'Layal'
+DELETE FROM students WHERE name='Layal'
