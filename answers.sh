50fce272fa9aa883f90669df2db28367c370c4da
Rolamaalouf <roula.maalouf@hotmail.fr>
2024-12-11 11:44:42 +0200

    exercise 3

--- answers.sh
+++ answers.sh
@@ -1,6 +1,6 @@
 exercise 1 
-select * from students
-select name from students
+SELECT * from students
+SELECT name from students
 SELECT * FROM students WHERE age > 30;
 SELECT name FROM students WHERE Gender = 'F' and age = 30
 SELECT points FROM students WHERE name = 'Alex'
@@ -14,4 +14,10 @@ DELETE FROM students WHERE name='Layal'
 exercise 3
 SELECT employees.Name  , employees.Company  ,companies.Date from employees,companies where employees.Company=companies.Name
 SELECT employees.Name from employees,companies where employees.Company=companies.Name and companies.Date<2000
-select Company from employees WHERE role='Graphic Designer'
+SELECTelect Company from employees WHERE role='Graphic Designer'
+exercise 4
+SELECT Name from students where Points=(select max(points) FROM students)
+SELECT avg(points)FROM students
+SELECT count(*)FROM students where points=500
+SELECT name FROM students WHERE Name like '%s%' 
+SELECT * FROM students ORDER by Points DESC
